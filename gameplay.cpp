37d56acaf13680449e9f099d529ff87f77c8d92b
wojlin <wojciech.linowski.2137@gmail.com>
2024-02-15 05:46:37 +0100

    working ship placing system

--- gameplay.cpp
+++ gameplay.cpp
@@ -18,135 +18,276 @@ void Gameplay::drawBoard()
 
 }
 
-void Gameplay::managePlacing()
+Gameplay::place Gameplay::getPlace()
 {
-    bool correct = false;
-    string shipType;
-    string position;
-    string rotation;
+    bool goodPlace = false;
+    place newPlace;
 
-    cout << endl;
-
-    while(!correct)
+    while(!goodPlace)
     {
-        
-        cout << "choose ship to place:";
-        if(shipsToPlace.size1 > 0)
-        {
-            cout << " <1>"; 
-        }
-        if(shipsToPlace.size2 > 0)
-        {
-            cout << " <2>"; 
-        }
-        if(shipsToPlace.size3 > 0)
-        {
-            cout << " <3>"; 
-        }
-        if(shipsToPlace.size4 > 0)
-        {
-            cout << " <4>"; 
-        }
-        cout << endl;
-        cin >> shipType;
+        bool correct = false;
+        string shipType;
+        string position;
+        string rotation;
 
-        try
+        int shipSize;
+        int xPos;
+        int yPos;
+        int shipRotation;
+
+        cout << endl;
+
+        while(!correct)
         {
-            int number = stoi(shipType);
-            if( number < 1 || number > 4)
+            
+            cout << "choose ship to place:";
+            if(shipsToPlace.size1 > 0)
+            {
+                cout << " <1>"; 
+            }
+            if(shipsToPlace.size2 > 0)
+            {
+                cout << " <2>"; 
+            }
+            if(shipsToPlace.size3 > 0)
+            {
+                cout << " <3>"; 
+            }
+            if(shipsToPlace.size4 > 0)
+            {
+                cout << " <4>"; 
+            }
+            cout << endl;
+            cin >> shipType;
+
+            try
+            {
+                int number = stoi(shipType);
+                shipSize = number;
+                if( number < 1 || number > 4)
+                {
+                    cerr << "you must choose a number between 1 and 4!" << endl;
+                    continue;
+                }
+
+                if(number == 1 && shipsToPlace.size1 == 0)
+                {
+                    cerr << "ships of size one are already used!" << endl;
+                    continue;
+                }
+                if(number == 2 && shipsToPlace.size2 == 0)
+                {
+                    cerr << "ships of size two are already used!" << endl;
+                    continue;
+                }
+                if(number == 3 && shipsToPlace.size3 == 0)
+                {
+                    cerr << "ships of size three are already used!" << endl;
+                    continue;
+                }
+                if(number == 4 && shipsToPlace.size4 == 0)
+                {
+                    cerr << "ships of size four are already used!" << endl;
+                    continue;
+                }
+
+
+                if(number == 1 && shipsToPlace.size1 > 0)
+                {
+                    correct = true;
+                    continue;
+                }
+                if(number == 2 && shipsToPlace.size2 > 0)
+                {
+                    correct = true;
+                    continue;
+                }
+                if(number == 3 && shipsToPlace.size3 > 0)
+                {
+                    correct = true;
+                    continue;
+                }
+                if(number == 4 && shipsToPlace.size4 > 0)
+                {
+                    correct = true;
+                    continue;
+                }
+
+
+            }
+            catch (...) 
             {
                 cerr << "you must choose a number between 1 and 4!" << endl;
                 continue;
             }
-
-            if(number == 1 && shipsToPlace.size1 == 0)
-            {
-                cerr << "ships of size one are already used!" << endl;
-                continue;
-            }
-            if(number == 2 && shipsToPlace.size2 == 0)
-            {
-                cerr << "ships of size two are already used!" << endl;
-                continue;
-            }
-            if(number == 3 && shipsToPlace.size3 == 0)
-            {
-                cerr << "ships of size three are already used!" << endl;
-                continue;
-            }
-            if(number == 4 && shipsToPlace.size4 == 0)
-            {
-                cerr << "ships of size four are already used!" << endl;
-                continue;
-            }
-
-
-            if(number == 1 && shipsToPlace.size1 > 0)
-            {
-                correct = true;
-                continue;
-            }
-            if(number == 2 && shipsToPlace.size2 > 0)
-            {
-                correct = true;
-                continue;
-            }
-            if(number == 3 && shipsToPlace.size3 > 0)
-            {
-                correct = true;
-                continue;
-            }
-            if(number == 4 && shipsToPlace.size4 > 0)
-            {
-                correct = true;
-                continue;
-            }
-
-
         }
-        catch (...) 
+
+    
+        correct = false;
+        while(!correct)
         {
-            cerr << "you must choose a number between 1 and 4!" << endl;
-            continue;
+            cout << "pick a spot to place ship of size " << shipType << ". example: <A4>,<G7> etc" << endl;
+            cin >> position;
+
+            if(position.size() != 2 && position.size() != 3)
+            {
+                cerr << "incorrect input!" << endl;
+                cerr << "positon format needs to be <letter><number> from A to J and 1 to 10!" << endl;
+                continue;
+            }
+
+
+
+            std::regex pattern("^[A-J]([1-9]|10)$");
+            bool check = std::regex_match(position, pattern);
+            
+            if(!check)
+            {
+                cerr << "positon format needs to be <letter><number> from A to J and 1 to 10!" << endl;
+                continue;
+            }
+
+
+            xPos = static_cast<int>(position.at(0) - 'A') + 1;
+
+            if(position.size() == 2)
+            {
+                yPos = position.at(1) - '0';
+            }
+            else
+            {
+                yPos = 10;
+            }
+
+            correct = true;
         }
-    }
 
-   
-    correct = false;
-    while(!correct)
-    {
-        cout << "pick a spot to place ship of size " << shipType << ". example: <A4>,<G7> etc" << endl;
-        cin >> position;
 
-        if(position.size() != 2 && position.size() != 3)
+        correct = false;
+        while(!correct)
         {
-            cerr << "incorrect input!" << endl;
-            cerr << "positon format needs to be <letter><number> from A to J and 1 to 10!" << endl;
-            continue;
+            cout << "do you want to place ship horizontal or vertical: h/v" << endl;
+            cin >> rotation;
+            if(rotation.size() != 1 || (rotation != "h" && rotation != "v"))
+            {
+                cerr << "you can only choose (h)orizontal or (v)ertical!" << endl;
+                continue;
+            }
+
+            if(rotation == "v")
+            {
+                shipRotation = 1;
+                correct = true;
+            }else
+            {
+                shipRotation = 0;
+                correct = true;
+            }
+
         }
 
-
-
-        std::regex pattern("^[A-J]([1-9]|10)$");
-        bool check = std::regex_match(position, pattern);
         
-        if(!check)
+        newPlace.shipSize = shipSize;
+        newPlace.posX = xPos - 1;
+        newPlace.posY = yPos - 1;
+        newPlace.rotate = shipRotation;
+
+        if(isValidPlace(newPlace))
         {
-            cerr << "positon format needs to be <letter><number> from A to J and 1 to 10!" << endl;
+            goodPlace = true;
+        }
+        else
+        {
+            cerr << "invalid position!" << endl;
             continue;
         }
 
-        //int xPos = static_cast<int>(firstChar - 'A') + 1;
-        //int yPos = secondChar - '0';
+        for(int i = 0; i < newPlace.shipSize; i++)
+        {
+            int partPosX = newPlace.posX + (i * (1 - newPlace.rotate));
+            int partPosY = newPlace.posY + (i * newPlace.rotate);
 
-        //cout << xPos << " " << yPos << endl;
+            playerBoard[partPosY][partPosX] = 1;
 
-        correct = true;
+        }
 
-
-
+        if(newPlace.shipSize == 1)
+        {
+            shipsToPlace.size1 -= 1;
+        }
+        if(newPlace.shipSize == 2)
+        {
+            shipsToPlace.size2 -= 1;
+        }
+        if(newPlace.shipSize == 3)
+        {
+            shipsToPlace.size3 -= 1;
+        }
+        if(newPlace.shipSize == 4)
+        {
+            shipsToPlace.size4 -= 1;
+        }
 
     }
+
+    return newPlace;
+}
+
+bool Gameplay::isValidPlace(Gameplay::place desiredPlace)
+{
+    // check for every ship part
+    for(int i = 0; i < desiredPlace.shipSize; i++)
+    {
+        int partPosX = desiredPlace.posX + (i * (1 - desiredPlace.rotate));
+        int partPosY = desiredPlace.posY + (i * desiredPlace.rotate);
+
+        for(int y = -1; y <= 1; y++)
+        {
+            for(int x = 0; x <= 1; x++)
+            {
+                int checkedPosX = partPosX  + x;
+                int checkedPosY = partPosY + y;
+
+                if( checkedPosX < 0 || checkedPosY < 0)
+                {
+                    continue;
+                }
+
+                if( checkedPosX > 9 || checkedPosY > 9)
+                {
+                    continue;
+                }
+
+                if(playerBoard[checkedPosY][checkedPosX] == 1)
+                {
+                    return false;
+                }
+
+            }
+        }
+        
+    }
+
+    return true;
+}
+
+void Gameplay::managePlacing()
+{
+    bool conditions = false;
+    while(!conditions)
+    {
+        place newPlace = getPlace();
+
+        
+
+        cout << "ship size: " << newPlace.shipSize << endl;
+        cout << "ship pos: " << newPlace.posX << " " << newPlace.posY << endl;
+        cout << "ship rotated:" << newPlace.rotate << endl;
+
+        drawPlacingBoard();
+    }
+
+    
     
     
 }
@@ -190,7 +331,14 @@ void Gameplay::drawPlacingBoard()
 
         for (int z = 0; z < 11; ++z) 
         {
-            cout << "║   ";
+            int yPos = i - 1;
+            int xPos = z;
+            string content = "   ";
+            if(playerBoard[yPos][xPos] == 1)
+            {
+                content = " x ";
+            }
+            cout << "║" << content;
         }
 
         cout << endl;
@@ -249,25 +397,25 @@ void Gameplay::drawPlacingBoard()
     cout << "\033[1B";
     cout << "\033[13D";
 
-    cout << "■■■■ " << "│ 1x";
+    cout << "■■■■ " << "│ "<< shipsToPlace.size4 <<"x";
     cout << "\033[1B";
     cout << "\033[9D";
-    cout << "     │   ";
+    cout << "     │    ";
     cout << "\033[1B";
     cout << "\033[9D";
-    cout << "■■■ " << " │ 2x";
+    cout << "■■■ " <<  "│ "<< shipsToPlace.size3 <<"x";
     cout << "\033[1B";
     cout << "\033[9D";
-    cout << "     │   ";
+    cout << "     │     ";
     cout << "\033[1B";
     cout << "\033[9D";
-    cout << "■■ " << "  │ 3x";
+    cout << "■■ " <<  "│ "<< shipsToPlace.size2 <<"x";
     cout << "\033[1B";
     cout << "\033[9D";
-    cout << "     │   ";
+    cout << "     │      ";
     cout << "\033[1B";
     cout << "\033[9D";
-    cout << "■ " << "   │ 4x";
+    cout << "■ " <<  "│ "<< shipsToPlace.size1 <<"x";
 
     cout << "\033[10B";
     cout << "\033[57D";
--- gameplay.h
+++ gameplay.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string.h>
 #include <regex>
+#include <vector>
 
 using namespace std;
 
@@ -20,17 +21,31 @@ public:
         int size1 = 4;
     };
 
+    struct place
+    {
+        int shipSize;
+        int posX;
+        int posY;
+        int rotate;
+    };
+
     ships playerShips;
     ships shipsToPlace;
-
+    std::vector<std::vector<int>> playerBoard = vector<vector<int>> (10, vector<int>(10, 0));
+    std::vector<std::vector<int>> enemyBoard = vector<vector<int>> (10, vector<int>(10, 0));
 
     void drawBoard();
 
     void drawPlacingBoard();
 
     void managePlacing();
+
+    place getPlace();
+
+    
     
 private:
+    bool isValidPlace(place desiredPlace);
 
 };
 
